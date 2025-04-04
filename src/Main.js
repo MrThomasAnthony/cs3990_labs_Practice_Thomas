4129095c05bb5698adb0e249d3e1e7aa7ddaeba2
MrThomasAnthony <thomaskelyn4@gmail.com>
2025-04-04 06:00:35 -0600

    prepared project by removing n/a files

--- src/Main.js
+++
@@ -1,8 +0,0 @@
-import { createButtons, displayButtonsWithDelay, createColorButton, showColorButton } from './myFunctions.js';
-
-// Create buttons
-const arrButtons = createButtons();
-
-// Display buttons with a 1-second delay
-createColorButton();
-displayButtonsWithDelay(arrButtons, 1000);
--- src/myArrays.js
+++
@@ -1,2 +0,0 @@
-export const arrTexts = ["Save Progress", "Click Me", "Download", "See More"];
-export const arrColors = ["Green", "Grey", "LightBlue", "Red"];
