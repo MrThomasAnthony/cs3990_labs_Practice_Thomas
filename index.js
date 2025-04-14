d3d1aeeb690198204fcc8d258eb3b63d9606e08b
MrThomasAnthony <thomaskelyn4@gmail.com>
2025-04-14 11:59:52 -0600

    added array of products

---
+++ index.js
@@ -0,0 +1,28 @@
+let hannaShop = [
+    {
+    group: "Coffee beans",
+    name: "Colombian Supremo Coffee",
+    price: 21.89,
+    image:
+    "https://assets.shop.loblaws.ca/products/21380579/b1/en/front/21380579_fro
+    },
+    {
+    group: "Coffee beans",
+    name: "Ethiopian Sidamo Coffee",
+    price: 23.49,
+    image: "https://www.ethiocoffee.ca/img/p/3/7/37-large_default.jpg",
+    },
+    {
+    group: "Coffee and Tea Accessories",
+    name: "Milk Frother",
+    price: 13.97,
+    image: "https://m.media-amazon.com/images/I/41woZkL5CKL._AC_US100_.jpg",
+    },
+    {
+    group: "Coffee makers",
+    name: "Braun Drip Coffee Maker",
+    price:
+    139,
+    image: "https://m.media-amazon.com/images/I/41N4HzzYZfL._AC_US100_.jpg",
+    },
+    ];
